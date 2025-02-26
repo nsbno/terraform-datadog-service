557595a0e82932d2c747cf24c429cc065b35cecf
Wictor Zhao <wictor_x@hotmail.com>
2025-02-26 14:01:01 +0100

    Handle null values in contacs

--- main.tf
+++ main.tf
@@ -3,6 +3,21 @@ data "aws_ssm_parameter" "team_name" {
 }
 
 locals {
+  contacts = [
+    for contact in [
+      var.support_email != null ? {
+        name    = "Support Email"
+        type    = "email"
+        contact = var.support_email
+      } : null,
+      var.slack_url != null ? {
+        name    = "Support Slack"
+        type    = "slack"
+        contact = var.slack_url
+      } : null,
+    ] : contact if contact != null
+  ]
+
   service_definition = {
     apiVersion = "v3"
     kind       = "service"
@@ -20,18 +35,7 @@ locals {
           url      = var.github_url
         } : null,
       ]
-      contacts = [
-        var.support_email != null ? {
-          name    = "Support Email"
-          type    = "email"
-          contact = var.support_email
-        } : null,
-        var.slack_url != null ? {
-          name    = "Support Slack"
-          type    = "slack"
-          contact = var.slack_url
-        } : null,
-      ]
+      contacts = length(local.contacts) > 0 ? local.contacts : null
     }
 
   }
