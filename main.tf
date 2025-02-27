ee6710467bc9cd1ff0b894133e0498d54a10a54f
Wictor Zhao <wictor_x@hotmail.com>
2025-02-27 09:57:18 +0100

    Revert "Properly handle contacts being null"
    This reverts commit cf04977326610d742289f225d9c3199134b4fcdf.

--- main.tf
+++ main.tf
@@ -4,41 +4,40 @@ data "aws_ssm_parameter" "team_name" {
 
 locals {
   contacts = [
-    var.support_email != null ? {
-      name    = "Support Email"
-      type    = "email"
-      contact = var.support_email
-    } : null,
-    var.slack_url != null ? {
-      name    = "Support Slack"
-      type    = "slack"
-      contact = var.slack_url
-    } : null,
-  ]
-
-  filtered_contacts = [for contact in local.contacts : contact if contact != null]
-
-  service_metadata = {
-    name        = var.service_name
-    displayName = var.display_name != null ? var.display_name : var.service_name
-    owner       = data.aws_ssm_parameter.team_name.value
-    description = var.description
-
-    tags = ["team:${data.aws_ssm_parameter.team_name.value}"]
-    links = [
-      var.github_url != null ? {
-        name     = "Source Code"
-        type     = "repo"
-        provider = "github"
-        url      = var.github_url
+    for contact in [
+      var.support_email != null ? {
+        name    = "Support Email"
+        type    = "email"
+        contact = var.support_email
       } : null,
-    ]
-  }
+      var.slack_url != null ? {
+        name    = "Support Slack"
+        type    = "slack"
+        contact = var.slack_url
+      } : null,
+    ] : contact if contact != null
+  ]
 
   service_definition = {
     apiVersion = "v3"
     kind       = "service"
-    metadata   = length(tolist(local.filtered_contacts)) > 0 ? merge(local.service_metadata, { contacts = local.filtered_contacts }) : local.service_metadata
+    metadata = {
+      name        = var.service_name
+      displayName = var.display_name != null ? var.display_name : var.service_name
+      owner       = data.aws_ssm_parameter.team_name.value
+      description = var.description
+      tags = ["team:${data.aws_ssm_parameter.team_name.value}"]
+      links = [
+          var.github_url != null ? {
+          name     = "Source Code"
+          type     = "repo"
+          provider = "github"
+          url      = var.github_url
+        } : null,
+      ]
+      contacts = length(local.contacts) > 0 ? local.contacts : null
+    }
+
   }
 }
 
