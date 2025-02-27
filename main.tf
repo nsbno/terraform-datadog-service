cf04977326610d742289f225d9c3199134b4fcdf
Wictor Zhao <wictor_x@hotmail.com>
2025-02-27 08:18:24 +0100

    Properly handle contacts being null

--- main.tf
+++ main.tf
@@ -4,40 +4,41 @@ data "aws_ssm_parameter" "team_name" {
 
 locals {
   contacts = [
-    for contact in [
-      var.support_email != null ? {
-        name    = "Support Email"
-        type    = "email"
-        contact = var.support_email
-      } : null,
-      var.slack_url != null ? {
-        name    = "Support Slack"
-        type    = "slack"
-        contact = var.slack_url
-      } : null,
-    ] : contact if contact != null
+    var.support_email != null ? {
+      name    = "Support Email"
+      type    = "email"
+      contact = var.support_email
+    } : null,
+    var.slack_url != null ? {
+      name    = "Support Slack"
+      type    = "slack"
+      contact = var.slack_url
+    } : null,
   ]
+
+  filtered_contacts = [for contact in local.contacts : contact if contact != null]
+
+  service_metadata = {
+    name        = var.service_name
+    displayName = var.display_name != null ? var.display_name : var.service_name
+    owner       = data.aws_ssm_parameter.team_name.value
+    description = var.description
+
+    tags = ["team:${data.aws_ssm_parameter.team_name.value}"]
+    links = [
+      var.github_url != null ? {
+        name     = "Source Code"
+        type     = "repo"
+        provider = "github"
+        url      = var.github_url
+      } : null,
+    ]
+  }
 
   service_definition = {
     apiVersion = "v3"
     kind       = "service"
-    metadata = {
-      name        = var.service_name
-      displayName = var.display_name != null ? var.display_name : var.service_name
-      owner       = data.aws_ssm_parameter.team_name.value
-      description = var.description
-      tags = ["team:${data.aws_ssm_parameter.team_name.value}"]
-      links = [
-          var.github_url != null ? {
-          name     = "Source Code"
-          type     = "repo"
-          provider = "github"
-          url      = var.github_url
-        } : null,
-      ]
-      contacts = length(local.contacts) > 0 ? local.contacts : null
-    }
-
+    metadata   = length(tolist(local.filtered_contacts)) > 0 ? merge(local.service_metadata, { contacts = local.filtered_contacts }) : local.service_metadata
   }
 }
 
